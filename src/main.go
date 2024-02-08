69875c3bbf7d86119d1e0338c597b9ea05825829
mydoom97 <73626499+mydoom97@users.noreply.github.com>
2024-02-08 15:45:28 +0800

    main

--- src/main.go
+++ src/main.go
@@ -6,12 +6,14 @@ import (
 
 var a = "Hello, World!"
 
-/*var b = int8(42)
-var c = int8(23)
-var d = int8(b)*/
-
 func main() {
+	var q int = 43
+	var d = 1
 	fmt.Println("Hello, World!")
 	fmt.Println(a)
+	fmt.Println(q)
+	fmt.Printf("a =%s\n", a)
+	fmt.Printf("%d\n", q)
+	fmt.Println(d)
 	//fmt.Printf("d == b+c", d, b, c)
 }
